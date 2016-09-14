8b9e39ba6ae0689066ad6d5beabded73ba475ac1
aurelienshz <aurelien.schiltz@polyconseil.fr>
2016-09-14 15:26:01 +0200

    Allow selecting a single file instead of a folder in conf file

--- src/bin/index.js
+++ src/bin/index.js
@@ -2,25 +2,29 @@
 /* eslint-disable no-console */
 
 const path = require('path');
+const fs = require('fs');
 
 const doks = require('../index.js');
+
+const DEFAULT_ROOT_FOLDER = path.join(process.cwd(), 'src');
 
 let doksConfig;
 try {
   doksConfig = require(path.join(process.cwd(), 'doks.config.js'));
 } catch (err) {
-  console.log('No doks.config.js found. Falling back to default settings');
+  console.log('WARNING : No doks.config.js found. Falling back to default settings');
+  console.log(err);
   doksConfig = {};
 }
 
 /**
- * Fetch doks.config.js config keys, or use our sensible defaults :
+ * Fetch doks.config.js config keys. When a conf key is not found, use a default one :
  */
 const config = {
   docsFolder: doksConfig.docsFolder || '__docs__',
-  defaultRoot: doksConfig.defaultRoot || path.join(process.cwd(), 'src'),
+  defaultRoot: doksConfig.defaultRoot || DEFAULT_ROOT_FOLDER,
   webpackConfig: doksConfig.webpackConfig || require(path.join(__dirname, '../../webpack.config.default.js')),
-  index: doksConfig.index || 'index.html',
+  index: doksConfig.index || path.join(__dirname, '..', '..', 'index.html'),
   serveStaticResources: doksConfig.serveStaticResources || [],
   staticBuildDir: doksConfig.staticBuildDir || path.join(process.cwd(), 'docs'),
   port: doksConfig.port || 3003,
@@ -53,6 +57,19 @@ if (args.length) {
     config.selectedPath = args[0];
   } else {
     config.selectedPath = path.join(cwd, args[0]);
+  }
+} else {
+  // No args config : we check if the root specified in the conf file is a file or a folder :
+  try {
+    // Path found :
+    const stats = fs.statSync(config.defaultRoot);
+    if (stats.isFile()) {
+      config.selectedPath = config.defaultRoot;
+    }
+  } catch (err) {
+    // Path not found :
+    console.warn('WARNING : ', config.contextPath, 'not found.\nUsing default root folder : ', config.defaultRoot);
+    config.selectedPath = DEFAULT_ROOT_FOLDER;
   }
 }
 
